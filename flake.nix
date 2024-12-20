898098bf77ccf7342628172ab169df6e4496865b
EstebanMarin <estebanmarin002@egmail.com>
2024-12-20 08:59:03 -0500

    cleaup

--- flake.nix
+++ flake.nix
@@ -7,27 +7,23 @@
   };
 
   outputs = { self, nixpkgs, nixpkgsveryold }: 
+
   let
     pkgs = nixpkgs.legacyPackages;
     pkgsold = nixpkgsveryold.legacyPackages;
   in {
     # Define packages
     packages = {
-#      x86_64-linux = pkgs.x86_64-linux.hello;
       aarch64-darwin = pkgs.aarch64-darwin.hello;
     };
 
     # Default package for 'nix run'
     defaultPackage = {
-#      x86_64-linux = self.packages.x86_64-linux;
-      aarch64-darwin = self.packages.aarch64-darwin;
+      aarch64-darwin = pkgs.aarch64-darwin.hello;
     };
 
     # Development shells
     devShells = {
-#      x86_64-linux = pkgs.x86_64-linux.mkShell {
-#        buildInputs = [ pkgs.x86_64-linux.hello ];
-#      };
       aarch64-darwin = pkgs.aarch64-darwin.mkShell {
         buildInputs = [ pkgs.neovim pkgsold.vim];
       };
