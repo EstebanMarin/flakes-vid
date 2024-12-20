3ef188f733d2d7cd4b7b6acebb10bcd683cfe91c
EstebanMarin <estebanmarin002@egmail.com>
2024-12-20 08:38:58 -0500

    making available only for darwin MacOS

--- flake.nix
+++ flake.nix
@@ -13,21 +13,21 @@
   in {
     # Define packages
     packages = {
-      x86_64-linux = pkgs.x86_64-linux.hello;
+#      x86_64-linux = pkgs.x86_64-linux.hello;
       aarch64-darwin = pkgs.aarch64-darwin.hello;
     };
 
     # Default package for 'nix run'
     defaultPackage = {
-      x86_64-linux = self.packages.x86_64-linux;
+#      x86_64-linux = self.packages.x86_64-linux;
       aarch64-darwin = self.packages.aarch64-darwin;
     };
 
     # Development shells
     devShells = {
-      x86_64-linux = pkgs.x86_64-linux.mkShell {
-        buildInputs = [ pkgs.x86_64-linux.hello ];
-      };
+#      x86_64-linux = pkgs.x86_64-linux.mkShell {
+#        buildInputs = [ pkgs.x86_64-linux.hello ];
+#      };
       aarch64-darwin = pkgs.aarch64-darwin.mkShell {
         buildInputs = [ pkgs.neovim pkgsold.vim];
       };
